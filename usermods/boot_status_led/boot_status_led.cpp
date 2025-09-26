f5ec0d2f2760bf18147fd5106dbc640803e0cbd7
GitHub Action <action@github.com>
2025-09-26 05:31:37 +0000

    updates required for 0.16.1-alpha

---
+++ usermods/boot_status_led/boot_status_led.cpp
@@ -0,0 +1,5 @@
+#include "boot_status_led.h"
+
+// Create and register the usermod instance
+BootStatusLEDUsermod bootStatusLEDUsermod;
+REGISTER_USERMOD(bootStatusLEDUsermod);
---
+++ usermods/wifi_status_led/wifi_status_led.cpp
@@ -0,0 +1,5 @@
+#include "wifi_status_led.h"
+
+// Create and register the usermod instance
+WiFiStatusLEDUsermod wifiStatusLEDUsermod;
+REGISTER_USERMOD(wifiStatusLEDUsermod);
